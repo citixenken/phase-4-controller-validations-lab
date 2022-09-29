0819e54e13b7ff31944c1cc9cea15bfa2fbe5657
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-09-29 07:25:49 +0300

    COMPLETED: Optional - Controller Validations Lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true # title cannot be blank
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction']}  # category is either 'Fiction' or 'non-fiction'
+    validates :content, presence: true, length: {minimum: 100} # content is at least 100 chars long
 end
